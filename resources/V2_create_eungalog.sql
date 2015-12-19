12c7c35719f19436f651b3f6d59fec69b2ddc793
hwiba <sungcheon.lee@outlook.com>
2015-12-19 23:35:32 +0900

    eungalog table 대소문자 수정 (for 배포)

--- resources/V2_create_eungalog.sql
+++ resources/V2_create_eungalog.sql
@@ -1,7 +1,7 @@
 drop table eungalog;
 
-CREATE TABLE `eungalog` (
-		`id` bigint PRIMARY KEY AUTO_INCREMENT
+CREATE TABLE eungalog (
+	id bigint PRIMARY KEY AUTO_INCREMENT
 	, user_name varchar(50)
 	, weather varchar(3) NOT null
 	, size varchar(10) not null
@@ -9,5 +9,5 @@ CREATE TABLE `eungalog` (
 	, satisfaction varchar(8) not null
 	, eunga_drip text
 	, createDate DATETIME DEFAULT CURRENT_TIMESTAMP
-	, FOREIGN KEY (user_name) REFERENCES users (name)
+	, FOREIGN KEY (user_name) REFERENCES USERS (name)
 );
